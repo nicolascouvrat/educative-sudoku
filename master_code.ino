f1ef1e878c9dd8eaafec5d3a8c49598f997eb55c
Nicolas <nicolas.couvrat@mines-paristech.fr>
2017-10-10 09:56:17 +0900

    initial commit

---
+++ master_code.ino
@@ -0,0 +1,312 @@
+
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include<Wire.h>
+
+using namespace std;
+
+/* 
+ *  This version uses 1D arrays to save memory space
+ *  Attribute POSITION is defined as position=column index*size+ row index
+ *  POSITION HAS TO BE INITIALIZED FROM 0 TO 15
+ *  /!\ with size=sqrt(list length) /!\
+ *  For given position, row=position%size and column=position/size
+*/
+
+/* 
+ *  COLOR CODE: 1=red, 2=green, 3=blue 4=yellow 0=NONE
+ *  REQUESTED DATA FORMAT : 3 characters in [color,adress] where address takes 2 characters from 01 to 16 
+ *  e.g. ['1','1','2'] = red on position 12
+ *  FINAL VERSION !!!!
+*/
+
+int*tab=new int[16]; //actual grid
+int*validity=new int[16]; //validity analysis 0=none, 1=valid or nothing(nothing valid by default)
+#define speakerPin A15 
+
+void setup() {
+  Wire.end();
+  Serial.begin(9600);
+  Wire.begin();
+  //fire up initial grid
+  tab=initializeSudoku();
+  
+  // light up the initial position
+  for(int i=0;i<16;i++){
+    validity[i]=1;
+    Wire.beginTransmission(i+1); 
+    Wire.write(tab[i]);
+    Wire.endTransmission(i+1);
+    delay(100);
+  }
+  displaySudoku(tab,4);
+
+}
+
+void loop() {
+  Serial.print("loop begin");
+
+for(int i=1;i<17;i++){
+  Serial.println("");
+  Wire.requestFrom(i,3);
+  char buf[3];
+  int j=0;
+  while (Wire.available()){ //might send less than expected
+    char c=Wire.read();
+    buf[j]=c;
+    j++;
+    }    
+  evaluate(buf); //check validity of position
+  delay(100); //TODO: modify delay?
+  }
+takeAction(); 
+Serial.print("loop end");
+}
+  
+
+bool isInList(int* list, int value, int listSize) {
+  // test for presence of value in list
+  for (int i = 0; i < listSize; i++) {
+    if (list[i] == value) return true;
+  }
+  return false;
+}
+
+bool isOnRow(int position, int value, int* grid, int size){
+  //line presence test
+  int rowIndex = position%size;
+  for (int i = rowIndex; i < size*size; i += size) {
+    if (grid[i] == value && i!=position) return true;
+  }
+  return false;
+}
+
+bool isOnColumn(int position, int value, int* grid, int size) {
+  //column presence test
+  int lineIndex = position / size;
+  for (int i = 0; i < size; i++) {
+    if (grid[lineIndex*size + i] == value && (lineIndex*size + i)!=position) return true;
+  }
+  return false;
+}
+
+bool isOnBlock(int position, int value, int* grid, int size) {
+  int const blockSize = 2;
+  // INTENDED FOR 4*4 GRID ONLY
+  // Modify accordingly for other dimensions
+  // block presence test
+  int blockPosition = blockSize*(int)((int)(position / size) / blockSize) + (int)((position%size) / blockSize);
+  int slide = 0;
+  switch (blockPosition) {
+  case 1:
+    slide = 2;
+    break;
+  case 2:
+    slide = 8;
+    break;
+  case 3:
+    slide = 10;
+    break;
+  }
+  for (int i = 0; i < blockSize; i++) {
+    if (grid[slide + i] == value && (slide + i)!=position) return true;
+    if (grid[slide + i + size] == value && (slide+i+size)!=position) return true;
+  }
+  return false;
+}
+
+void displaySudoku(int* grid, int size) {
+  //displays content of grid on screen
+  //test method
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      Serial.print(grid[j+i*size]);
+    }
+    Serial.println("");
+  }
+  Serial.println("DONE");
+}
+int* copyGrid(int* grid, int size) {
+  //copies a grid to a "virtual" one
+  int *newGrid = new int[size*size];
+  for (int i = 0; i < size*size; i++) {
+    newGrid[i] = grid[i];
+  }
+  return newGrid;
+}
+
+bool backTrack(int* grid, int position, int size) { 
+  //solve grid using backtrack method
+  //where position=column index*size+row index
+  if (position == size*size) {
+    return true;
+  }//all grid has been screened, job done
+  if (grid[position] != 0) {
+    return backTrack(grid, position + 1, size); //moving on if already filled
+  }
+  for (int i = 1; i < size + 1; i++) { //recursive part
+    if (!isOnColumn(position, i, grid, size)&& !isOnRow(position, i, grid, size) && !isOnBlock(position, i, grid, size)) {
+      grid[position] = i;
+      if (backTrack(grid, position + 1, size)){
+        return true;
+      }
+    }
+  }
+  grid[position] = 0;
+  return false;
+}
+
+bool backTrackWithCondition(int* grid, int position, int size, int forbiddenValue, int forbiddenPosition) {
+  //solver with condition (one specific value at a specific position is forbidden)
+  //where position=column index*size+row index
+  //used to see if there is more than one solution for a given position
+  if (position == size*size) {
+    return true;
+  }//all grid has been screened, job done
+  if (grid[position] != 0) {
+    return backTrackWithCondition(grid, position + 1, size, forbiddenValue, forbiddenPosition); //moving on if already filled
+  }
+  for (int i = 1; i < size + 1; i++) { //recursive part
+    if (!(i == forbiddenValue) || !(position == forbiddenPosition)) {
+      {
+        if (!isOnColumn(position, i, grid, size) && !isOnRow(position, i, grid, size) && !isOnBlock(position, i, grid, size)) {
+          grid[position] = i;
+          if (backTrackWithCondition(grid, position + 1, size, forbiddenValue, forbiddenPosition)) {
+            return true;
+          }
+        }
+      }
+    }
+  }
+  grid[position] = 0;
+  return false;
+}
+
+
+int* generateSudoku(int size) {
+  //create empty grid
+  int *grid = new int[16];
+  for (int i = 0; i < size*size; i++) {
+    grid[i] = 0;
+  }
+  return grid;
+}
+
+void addRandomNumber(int position, int* grid, int size) {
+  //add random number to specified position respecting sudoku rules
+  srand(analogRead(0));
+  int value = 0;
+  do {
+    value = rand() % size + 1;
+  } while (isOnBlock(position, value, grid, size) || isOnRow(position, value, grid, size) || isOnColumn(position, value, grid, size));
+  grid[position] = value;
+}
+
+int* createRandomFilled(int size) {
+  //create a random valid grid fully filled
+  int* grid = generateSudoku(size);
+  for (int position = 0; position < size*size; position++) {
+    do {
+      addRandomNumber(position, grid, size);
+    } while (!backTrack(copyGrid(grid, size), 0, size));
+  }
+  return grid;
+}
+
+int generateRandom(int max) {
+  // generate random int in (0, max)
+  srand(analogRead(0));
+  return rand() % (max - 1);
+}
+
+int* clearSudoku(int* validGrid,int size) {
+  // progressively clears a previously fully filled valid grid
+  // stops when you cannot take out any additional numbers without breaking the unicity of the solution
+  // (so there is no need for guessing during solving)
+  bool pursue = true;
+  int toRemove = 0;
+  int toStore = 0; //store the removed value if backup need
+  int* alreadyTried = new int[16];
+  int storeIndex = 0;
+  while (pursue) {
+    do {
+      toRemove = generateRandom(16);
+    } while (isInList(alreadyTried,toRemove,16));
+    toStore = validGrid[toRemove]; //prepare backup
+    validGrid[toRemove] = 0;
+    
+    if (backTrackWithCondition(copyGrid(validGrid, size), 0, size, toStore, toRemove)) {
+      validGrid[toRemove] = toStore;
+      break;
+    }
+    alreadyTried[storeIndex] = toRemove;
+    storeIndex++;
+    Serial.print("");
+  }
+  return validGrid;
+}
+
+int* initializeSudoku(){
+  // creates the starting position
+  int* temp=new int[16];
+  temp=createRandomFilled(4); //we always want a 4*4 grid anyway
+  temp=clearSudoku(temp,4);
+  return temp;
+}
+
+void evaluate(char c[]){
+  /*
+   * pos from 0 to 15, value from 0 to 4, 0 being none
+   * this function updates the validity grid
+   */
+  int value=(int) (c[0]-'0')%48; //to int
+  int posHigh=(int)(c[1]-'0')%48;
+  int posLow=(int)(c[2]-'0')%48;
+  int pos=posLow+10*posHigh-1;
+  if(value!=0){
+    if(!isOnRow(pos,value,tab,4)&&!isOnColumn(pos,value,tab,4)&&!isOnBlock(pos,value,tab,4)){
+      validity[pos]=1;
+    } else{
+      validity[pos]=0;
+    }
+  } else{
+    validity[pos]=1; //if nothing then it is valid anyway
+  }
+ tab[pos]=value;
+}
+
+bool fullValid(){
+  for(int i=0;i<16;i++){
+    if (validity[i]==0) return false;
+  }
+  return true;
+}
+
+bool isOver(){
+  for(int i=0;i<16;i++){
+    if (tab[i]==0) return false;
+  }
+  return true;
+}
+void takeAction(){
+  if(!fullValid()) {
+    Serial.println("WRONG");
+    tone(speakerPin, 277, 100);
+    delay(120);
+    tone(speakerPin, 277, 500);
+    delay(500);
+  }
+  if(fullValid()&&isOver()){
+    tone(speakerPin, 1760, 200);
+    delay(100);
+    tone(speakerPin, 1397, 200);
+    delay(100);
+    tone(speakerPin, 1760, 200);
+    delay(100);
+    tone(speakerPin, 1397, 500);
+    delay(500);
+  }
+  //TODO: turn on buzzer
+}
+
---
+++ sensor_code.ino
@@ -0,0 +1,210 @@
+// Arduinoのピン定義 http://garretlab.web.fc2.com/arduino/lab/digital_color_sensor/index.html
+//ピンの配置　http://kousaku-kousaku.blogspot.jp/2008/11/arduinos9706.html
+//繋げ方　https://fabble.cc/arduinotutorial/colorsensor
+
+/*
+ * Code version 2017/1/23
+ * This is the sensor + color decision code 
+ * GENERAL CODE
+ */
+#include <Wire.h>
+const int doutPin = 12;   // Dout
+const int rangePin = 9;  // Range
+const int ckPin = 10;     // CK
+const int gatePin =11;   // Gate
+int pinRed=3; //pin values for LED coloring
+int pinBlue=5; //TODO: Change these pin values
+int pinGreen=4;
+int address=14; // TO CHANGE DEPENDING ON POSITION
+
+int addressHigh=0; //updated according to address at setup
+int addressLow=0;
+
+const int trigPin = A0;
+const int echoPin = A1;
+
+int currentColor=0; //COLOR CODE: 1=pink, 2=green, 3=blue 4=grey 0=NONE , determined by sensor OR set up by master
+bool initialSetUp=false; // if the color has been set up by the master, we prevent it from changing later
+
+
+void setup() {
+  addressHigh=(address/10);
+  addressLow=(address%10);
+  Wire.begin(address);
+  Serial.begin(9600);
+  Serial.println(addressLow);
+  Serial.println(addressHigh);
+  // ピンモードを設定する。doutPinは入力、それ以外は出力。
+  pinMode(doutPin, INPUT);
+  pinMode(rangePin, OUTPUT);
+  pinMode(ckPin, OUTPUT);
+  pinMode(gatePin, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(pinRed,OUTPUT);
+  pinMode(pinBlue,OUTPUT);
+  pinMode(pinGreen,OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestedEvent);
+}
+
+void loop() {
+  if(!initialSetUp){ //if it has been set up by master then when don't bother running through the color decision code
+    if(distanceCheck()){
+        digitalWrite(13, HIGH);   // set the LED on
+        // put your main code here, to run repeatedly: 
+        int red, green, blue;  // 測定した値を格納するための変数
+        int integration = 50;  // 測定時間(ミリ秒)を格納する変数
+        char s[64];            // シリアルコンソールに出力する文字列を保持する変数
+        
+        digitalWrite(gatePin, LOW);        // GateとCKをLOWにする。
+        digitalWrite(ckPin, LOW);
+        digitalWrite(rangePin, HIGH);      // RangeをHIGHにする。
+        
+        digitalWrite(gatePin, HIGH);       // GateをHIGHにして測定開始。
+        delay(integration);                // 測定時間だけ待つ。
+        digitalWrite(gatePin, LOW);        // GateをLOWにして測定終了。
+        delayMicroseconds(4);              // 4ミリ秒待つ。
+        red = shiftIn12(doutPin, ckPin);   // 赤色の値を読む。
+        delayMicroseconds(3);              // 3ミリ秒待つ。
+        green = shiftIn12(doutPin, ckPin); // 緑色の値を読む。
+        delayMicroseconds(3);              // 3ミリ秒待つ。
+        blue = shiftIn12(doutPin, ckPin);  // 青色の値を読む。
+        
+        sprintf(s, "%4d, %4d, %4d", red, green, blue);
+        Serial.println(s);
+        if(6.700+-210.400*red+32.700*green+38.400*blue>0){
+          Serial.println("blue");
+          currentColor=3;
+        }
+        else if(0.000+20.300*red+-74.700*green+26.800*blue>0){
+          Serial.println("pink");
+          currentColor=2;
+        }
+        else if(8.900+-1.000*red+62.000*green+-88.100*blue>0){
+          Serial.println("green");
+          currentColor=1;
+        }
+        else if(12.200+40.100*red+-8.100*green+-35.800*blue>0){
+          Serial.println("grey");
+          currentColor=4;
+        }
+        else{
+          Serial.println("etc...");
+          currentColor=0;
+        }  
+    }else{
+      currentColor=0;
+    }    
+  }
+  else{
+    if(distanceCheck()) setUpColor(0);
+    else setUpColor(currentColor);
+  }
+  delay(500);
+}
+
+//12ビットの値を読み込む関数(LSBから送信されるデータを想定)
+int shiftIn12(int dataPin, int clockPin) {
+  int value = 0;
+  
+  for (int i = 0; i < 12; i++) {
+    digitalWrite(clockPin, HIGH);           // クロックをHIGHにする
+    value |= digitalRead(dataPin) << i;     // データピンの値を読み取り、所定のビットを設定する。
+    digitalWrite(clockPin, LOW);            // クロックピンをLOWにする。
+  }
+ 
+  return value;
+}
+
+/*
+ * FROM HERE, NON COLOR-SENSOR RELATED CODE
+ */
+void setUpColor(int receivedValue){
+  /*
+   * COLOR CODE: 1=green, 2=pink, 3=blue 4=brown/orange 0=NONE
+   */
+  switch(receivedValue){
+    case 2: 
+    setColor(200, 100, 40);  // pink
+    break;
+    case 1: 
+    setColor(0, 255, 0);  // green
+    break;
+    case 3:
+    setColor(0, 0, 255);  // blue
+    break;
+    case 4:
+    setColor(255, 255, 255);  // orange (lqst color)
+    break;
+    default:
+    setColor(0,0,0); //clear
+    break;
+  }
+}
+
+void receiveEvent(int bytes){
+  initialSetUp=false;
+  currentColor=0;
+  //receive color info for initial setup
+  currentColor=Wire.read();
+  //Serial.println(currentColor); //to test received value
+  setUpColor(currentColor); //turn on the led according to the received value
+  if(currentColor!=0) initialSetUp=true; //lock the color because it has been decided by master IF ITS NOT NILL
+}
+
+void requestedEvent(){
+  /*
+   * REQUESTED DATA FORMAT : 3 characters in [color,adress] where address takes 2 characters from 01 to 16 
+   */
+  char Buf[3];
+  Buf[0]='0'+currentColor; //risk of bug here?
+  Buf[1]='0'+addressHigh;
+  Buf[2]='0'+addressLow;
+  Wire.write(Buf); //respond with msg
+}
+
+void setColor(int red, int green, int blue) {
+  analogWrite(pinRed, red);
+  analogWrite(pinGreen, green);
+  analogWrite(pinBlue, blue);  
+}
+
+bool distanceCheck(){
+    // establish variables for duration of the ping, 
+  // and the distance result in inches and centimeters:
+  long duration, inches, cm;
+
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  
+  duration = pulseIn(echoPin, HIGH);
+
+  // convert the time into a distance
+  cm = microsecondsToCentimeters(duration);
+  Serial.println(cm);
+  if(cm<=4) return true;
+  else digitalWrite(13,LOW);
+  return false;
+  //return true; //FOR TEST PURPOSE, REMOVE THIS LINE
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
+
